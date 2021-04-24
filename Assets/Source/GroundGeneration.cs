860b1916ffd6b2ad3835b13b21706ac7c1d50997
jacopo grandi <jak.sk8@hotmail.it>
2021-04-24 20:58:02 +0200

    rules

--- Assets/Source/GroundGeneration.cs
+++ Assets/Source/GroundGeneration.cs
@@ -3,6 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
+public class Rule {
+    public int[] match;
+    public TileBase[] tile;
+    public Matrix4x4 matrix;
+}
+
 public class Room {
     public List<Vector3Int> cells = new List<Vector3Int>();
     public Vector2Int boundsTopLeft;
@@ -10,12 +16,11 @@ public class Room {
     public List<Room> neighbors = new List<Room>();
     public bool hasMoss = false;
 
-    public bool Overlap (Room oth) {
+    public bool Overlap(Room oth) {
         if (boundsTopLeft.x > oth.boundsBottomRight.x
             && boundsBottomRight.x < oth.boundsTopLeft.x
             && boundsTopLeft.y > oth.boundsBottomRight.y
-            && boundsBottomRight.y < oth.boundsTopLeft.y)
-        {
+            && boundsBottomRight.y < oth.boundsTopLeft.y) {
             return false;
         }
         foreach (Vector3Int apt in cells) {
@@ -23,12 +28,12 @@ public class Room {
         }
         return false;
     }
-    public float SqrDistance (Room oth, out Vector3Int from, out Vector3Int to) {
+    public float SqrDistance(Room oth, out Vector3Int from, out Vector3Int to) {
         float dist = float.PositiveInfinity;
         from = new Vector3Int(); to = new Vector3Int(); // c# 
         foreach (Vector3Int pt in cells) {
             foreach (Vector3Int othpt in oth.cells) {
-                Vector2 diff = new Vector2(pt.x-othpt.x, pt.y-othpt.y);
+                Vector2 diff = new Vector2(pt.x - othpt.x, pt.y - othpt.y);
                 if (diff.sqrMagnitude < dist) {
                     dist = diff.sqrMagnitude;
                     from = pt; to = othpt;
@@ -38,7 +43,7 @@ public class Room {
         return dist;
     }
 
-    public List<Vector3Int> Contour () {
+    public List<Vector3Int> Contour() {
         List<Vector3Int> cont = new List<Vector3Int>();
         foreach (Vector3Int cell in cells) {
             bool border = false;
@@ -59,16 +64,22 @@ public class Room {
 public class GroundGeneration : MonoBehaviour {
 
     public Vector2Int size;
-    public TileBase groundTile;
-    public TileBase corridorTile;
-    public TileBase roomTile;
-    public TileBase mossTile;
+    public TileBase ground_full_0Tile;
+    public TileBase ground_full_1Tile;
+    public TileBase ground_corridorTile;
+    public TileBase ground_corridor_cornerTile;
+    public TileBase ground_roomTile;
+    public TileBase moss_0Tile;
+    public TileBase moss_1Tile;
+    public TileBase moss_2Tile;
+    public TileBase moss_cornerTile;
+    public TileBase moss_corner_innerTile;
     public Tilemap groundtilemap;
     public Tilemap wallstilemap;
     public List<Room> rooms = new List<Room>();
     public Room start;
 
-    Room GenRoomRandRect(int size, int n=8) {
+    Room GenRoomRandRect(int size, int n = 8) {
         Room room = new Room();
         for (int i = 0; i < n; i++) {
             int sx = Random.Range(2, size);
@@ -94,7 +105,7 @@ public class GroundGeneration : MonoBehaviour {
                     if (!room.cells.Contains(pt)) {
                         room.cells.Add(pt);
                     }
-                } 
+                }
             }
         }
         room.boundsTopLeft = new Vector2Int(-size, -size);
@@ -102,7 +113,7 @@ public class GroundGeneration : MonoBehaviour {
         return room;
     }
 
-    void DrawCorridor (Vector3Int from, Vector3Int to) {
+    void DrawCorridor(Vector3Int from, Vector3Int to) {
         Vector3Int[] dirToVec = new Vector3Int[4] {
             new Vector3Int(1, 0, 0),
             new Vector3Int(0, 1, 0),
@@ -110,6 +121,7 @@ public class GroundGeneration : MonoBehaviour {
             new Vector3Int(0, -1, 0)
         };
         int iter = 0;
+        List<Vector3Int> corr = new List<Vector3Int>();
         for (; iter < 100; iter++) {
             if (from == to) break;
             float mind = float.PositiveInfinity;
@@ -123,17 +135,82 @@ public class GroundGeneration : MonoBehaviour {
 
             from = from + dirToVec[seld];
             wallstilemap.SetTile(from, null);
-            groundtilemap.SetTile(from, corridorTile);
+            corr.Add(from);
+        }
+
+        List<Rule> rules = new List<Rule>();
+        {
+            Rule rule = new Rule();
+            rule.match = new int[4] { 1, 0, 1, 0 };
+            rule.tile = new TileBase[] { ground_corridorTile };
+            rule.matrix = Matrix4x4.TRS(Vector3.zero, Quaternion.Euler(0f, 0f, 90f), Vector3.one);
+            rules.Add(rule);
+        }
+        {
+            Rule rule = new Rule();
+            rule.match = new int[4] { 0, 1, 0, 1 };
+            rule.tile = new TileBase[] { ground_corridorTile };
+            rule.matrix = Matrix4x4.TRS(Vector3.zero, Quaternion.Euler(0f, 0f, 0f), Vector3.one);
+            rules.Add(rule);
+        }
+        {
+            Rule rule = new Rule();
+            rule.match = new int[4] { 1, 1, 0, 0 };
+            rule.tile = new TileBase[] { ground_corridor_cornerTile };
+            rule.matrix = Matrix4x4.TRS(Vector3.zero, Quaternion.Euler(0f, 0f, 90f), Vector3.one);
+            rules.Add(rule);
+        }
+        {
+            Rule rule = new Rule();
+            rule.match = new int[4] { 1, 0, 0, 1 };
+            rule.tile = new TileBase[] { ground_corridor_cornerTile };
+            rule.matrix = Matrix4x4.TRS(Vector3.zero, Quaternion.Euler(0f, 0f, 0f), Vector3.one);
+            rules.Add(rule);
+        }
+        {
+            Rule rule = new Rule();
+            rule.match = new int[4] { 0, 1, 1, 0 };
+            rule.tile = new TileBase[] { ground_corridor_cornerTile };
+            rule.matrix = Matrix4x4.TRS(Vector3.zero, Quaternion.Euler(0f, 0f, 180f), Vector3.one);
+            rules.Add(rule);
+        }
+        {
+            Rule rule = new Rule();
+            rule.match = new int[4] { 0, 0, 1, 1 };
+            rule.tile = new TileBase[] { ground_corridor_cornerTile };
+            rule.matrix = Matrix4x4.TRS(Vector3.zero, Quaternion.Euler(0f, 0f, 270f), Vector3.one);
+            rules.Add(rule);
+        }
+
+
+        for (int i = 0; i < corr.Count; i++) {
+            foreach (Rule rule in rules) {
+                bool found = true;
+                for (int d = 0; d < 4; d++) {
+                    Vector3Int see = corr[i] + dirToVec[d];
+                    if (rule.match[d] == 0) {
+                        if (corr.Contains(see)) {
+                            found = false;
+                            break;
+                        }
+                    }
+                }
+                if (found) {
+                    groundtilemap.SetTile(corr[i], rule.tile[0]);
+                    groundtilemap.SetTransformMatrix(corr[i], rule.matrix);
+                    break;
+                }
+            }
         }
         if (iter >= 100) { print("out of iteration"); }
     }
 
-    void ConnectRooms (List<Room> rooms) {
+    void ConnectRooms(List<Room> rooms) {
         Room start = rooms[0];
         List<Room> conn = new List<Room>();
         conn.Add(start);
         int iter = 0;
-        for (; iter < rooms.Count-1; iter+= 1) {
+        for (; iter < rooms.Count - 1; iter += 1) {
             float mind = float.PositiveInfinity;
             Vector3Int from = new Vector3Int(), to = new Vector3Int();
             Room nearest = null;
@@ -164,7 +241,7 @@ public class GroundGeneration : MonoBehaviour {
         }
     }
 
-    void GroundCorridors (List<Room> rooms, int n) {
+    void GroundCorridors(List<Room> rooms, int n) {
         EnemyManagement em = FindObjectOfType<EnemyManagement>();
         Room start = rooms[0];
         {
@@ -182,7 +259,7 @@ public class GroundGeneration : MonoBehaviour {
 
         rooms.Sort((a, b) => a.cells[0].y.CompareTo(b.cells[0].y));
         rooms.Reverse();
-        for (int i=0; i<n; i++) {
+        for (int i = 0; i < n; i++) {
             Room temp = new Room();
             Vector3Int spawn = new Vector3Int(rooms[i].cells[0].x + 4, 0, 0);
             SpawnPoint sp = new SpawnPoint();
@@ -196,17 +273,19 @@ public class GroundGeneration : MonoBehaviour {
         rooms.Add(start);
     }
 
-    void Gen (int n) {
-        for (int x = -size.x/2; x < size.x/2; x++) {
+    void Gen(int n) {
+        for (int x = -size.x / 2; x < size.x / 2; x++) {
             for (int y = 0; y < size.y; y++) {
-                wallstilemap.SetTile(new Vector3Int(x, -y, 0), groundTile);
+                TileBase tile = Random.Range(0, 2) == 0 ?
+                    ground_full_0Tile : ground_full_1Tile;
+                wallstilemap.SetTile(new Vector3Int(x, -y, 0), tile);
             }
         }
 
         start = GenRoomRandRect(8, 12);
         Vector3Int soff = new Vector3Int(0, -9, 0);
         for (int j = 0; j < start.cells.Count; j++) { start.cells[j] += soff; }
-        rooms.Add(start); n -= 1; 
+        rooms.Add(start); n -= 1;
 
         for (int i = 0; i < n; i++) {
             Room r = null;
@@ -224,7 +303,7 @@ public class GroundGeneration : MonoBehaviour {
                     float dist = r.SqrDistance(oth, out from, out to);
                     mind = Mathf.Min(mind, dist);
                 }
-                if (mind < 5*5 && mind > 3*3) {
+                if (mind < 5 * 5 && mind > 3 * 3) {
                     break; // success
                 }
             }
@@ -237,7 +316,7 @@ public class GroundGeneration : MonoBehaviour {
             foreach (Vector3Int pt in r.cells) {
                 if (wallstilemap.HasTile(pt)) {
                     wallstilemap.SetTile(pt, null);
-                    groundtilemap.SetTile(pt, roomTile);
+                    groundtilemap.SetTile(pt, ground_roomTile);
                 }
             }
         }
@@ -267,11 +346,88 @@ public class GroundGeneration : MonoBehaviour {
         CoverWithMoss(start);
     }
 
-    public void CoverWithMoss (Room room) {
+    public void CoverWithMoss(Room room) {
+
+        List<Rule> rules = new List<Rule>();
+        {
+            Rule rule = new Rule();
+            rule.match = new int[9] {
+                0, 0, 0,
+                1, 1, 1,
+                0, 2, 0
+            };
+            rule.tile = new TileBase[] { moss_0Tile, moss_1Tile, moss_2Tile, };
+            rule.matrix = Matrix4x4.TRS(Vector3.zero, Quaternion.Euler(0f, 0f, 0f), Vector3.one);
+            rules.Add(rule);
+        }/*
+        {
+            Rule rule = new Rule();
+            rule.match = new int[9] {
+                0, 0, 0,
+                0, 1, 1,
+                0, 1, 2
+            };
+            rule.tile = new TileBase[] { moss_cornerTile, };
+            rule.matrix = Matrix4x4.TRS(Vector3.zero, Quaternion.Euler(0f, 0f, 0f), Vector3.one);
+            rules.Add(rule);
+        }
+        {
+            Rule rule = new Rule();
+            rule.match = new int[9] {
+                0, 1, 0,
+                1, 1, 0,
+                0, 0, 2
+            };
+            rule.tile = new TileBase[] { moss_corner_innerTile, };
+            rule.matrix = Matrix4x4.TRS(Vector3.zero, Quaternion.Euler(0f, 0f, 0f), Vector3.one);
+            rules.Add(rule);
+        }*/
+
+        List<Rule> rotatedRules = new List<Rule>();
+        foreach (Rule rule in rules) {
+            for (int i = 0; i < 4; i++) {
+                Rule rot = new Rule();
+                rot.match = new int[9] {
+                    rule.match[2], rule.match[5], rule.match[8],
+                    rule.match[1], rule.match[4], rule.match[7],
+                    rule.match[0], rule.match[3], rule.match[6]
+                };
+                rot.tile = rule.tile;
+                rot.matrix = Matrix4x4.TRS(Vector3.zero, Quaternion.Euler(0f, 0f, 90f * i), Vector3.one);
+                rotatedRules.Add(rot);
+            }
+        }
+
         room.hasMoss = true;
         List<Vector3Int> border = room.Contour();
         foreach (Vector3Int b in border) {
-            groundtilemap.SetTile(b + new Vector3Int(0, 0, 1), mossTile);
+            foreach (Rule rule in rotatedRules) {
+                bool found = true;
+                for (int j = -1; j < 2; j++) {
+                    for (int i = -1; i < 2; i++) { 
+                        Vector3Int see = b + new Vector3Int(i, j, 0);
+                        int xy = i + 1 + (j + 1) * 3;
+                        if (rule.match[xy] == 1) {
+                            if (!border.Contains(see)) {
+                                found = false;
+                                break;
+                            }
+                        }
+                        if (rule.match[xy] == 2) {
+                            if (!wallstilemap.GetTile(see)) {
+                                found = false;
+                                break;
+                            }
+                        }
+                    }
+                }
+                if (found) {
+                    TileBase tile = rule.tile[Random.Range(0, rule.tile.Length - 1)];
+                    groundtilemap.SetTile(b + new Vector3Int(0, 0, 1), tile);
+                    groundtilemap.SetTransformMatrix(b + new Vector3Int(0, 0, 1), rule.matrix);
+                    break;
+                }
+            }
         }
     }
 }
